abac40d6c405f1470b477265ac7eef6643c4a201
Mahmoud Elkordy <mahmoud_elkordy65@yahoo.com>
2021-04-04 15:07:06 +0200

    application logic completed

--- app.js
+++ app.js
@@ -1,79 +1,98 @@
-var flag = false;
+/* we cant add functions for simplification :) */
+let flag = true;
 
-
-
-switch (flag) {
-  case false:
-    hoverO();
-    firstOperationForO();
-    break;
-  case true:
-    hoverX();
-    firstOperationForX();
-    break;
-  default:
-    alert("error");
-}
-
-
-
-
-
-
-
-
-
-/**
- * hoverX - this function is used to show the (x) sign on the square you are hovering on.
- * @return {type}  description
- */
-function hoverX() {
-  $(".col").mouseenter(function() {
+$(".col").mouseenter(function() {
+  if (flag === true) {
     $(this).addClass("add-x-image");
-  });
-
-  $(".col").mouseleave(function() {
-    $(this).removeClass("add-x-image");
-  });
-}
-
-/**
- * hoverO - this function is used to show the (O) sign on the square you are hovering on.
- *
- * @return {type}  description
- */
-function hoverO() {
-  $(".col").mouseenter(function() {
+  } else {
     $(this).addClass("add-o-image");
-  });
+  }
+});
 
-  $(".col").mouseleave(function() {
+$(".col").mouseleave(function() {
+  if (flag === true) {
+    $(this).removeClass("add-x-image");
+  } else {
     $(this).removeClass("add-o-image");
-  });
+  }
+});
+
+$(".col").click(function() {
+  if (flag === true) {
+    if ($(this).hasClass('x-image') || $(this).hasClass('o-image')) {
+      // do nothing
+    } else {
+      $(this).addClass('x-image').removeClass('add-x-image');
+      xWins();
+      flag = false;
+    }
+  } else {
+    if ($(this).hasClass('x-image') || $(this).hasClass('o-image')) {
+      // do nothing
+    } else {
+      $(this).addClass('o-image').removeClass('add-o-image');
+      oWins();
+      flag = true;
+    }
+  }
+});
+
+function xWins(){
+  let div1X = $('.div-1').hasClass('x-image');
+  let div2X = $('.div-2').hasClass('x-image');
+  let div3X = $('.div-3').hasClass('x-image');
+  let div4X = $('.div-4').hasClass('x-image');
+  let div5X = $('.div-5').hasClass('x-image');
+  let div6X = $('.div-6').hasClass('x-image');
+  let div7X = $('.div-7').hasClass('x-image');
+  let div8X = $('.div-8').hasClass('x-image');
+  let div9X = $('.div-9').hasClass('x-image');
+
+  if ( div1X && div2X && div3X ) {
+    alert("X`s win");
+  }else if( div4X && div5X && div6X ) {
+    alert("X`s win");
+  }else if( div7X && div8X && div9X ) {
+    alert("X`s win");
+  }else if( div1X && div4X && div7X ) {
+    alert("X`s win");
+  }else if( div2X && div5X && div8X ) {
+    alert("X`s win");
+  }else if( div3X && div6X && div9X ) {
+    alert("X`s win");
+  }else if( div1X && div5X && div9X ) {
+    alert("X`s win");
+  }else if( div3X && div5X && div7X ) {
+    alert("X`s win");
+  }
 }
 
-/**
- * firstOperationForX - used to put (x) sign on the box you have clicked on.
- *
- * @return {flag}  description
- */
-function firstOperationForX() {
-  $(".col").click(function() {
-    $(this).addClass('x-image');
-  });
-  flag = false;
-  return flag;
-}
+function oWins(){
+  let div1O = $('.div-1').hasClass('o-image');
+  let div2O = $('.div-2').hasClass('o-image');
+  let div3O = $('.div-3').hasClass('o-image');
+  let div4O = $('.div-4').hasClass('o-image');
+  let div5O = $('.div-5').hasClass('o-image');
+  let div6O = $('.div-6').hasClass('o-image');
+  let div7O = $('.div-7').hasClass('o-image');
+  let div8O = $('.div-8').hasClass('o-image');
+  let div9O = $('.div-9').hasClass('o-image');
 
-/**
- * firstOperationForO - used to put (O) sign on the box you have clicked on.
- *
- * @return {type}  description
- */
-function firstOperationForO() {
-  $(".col").click(function() {
-    $(this).addClass('o-image');
-  });
-  flag = true;
-  return flag;
+  if ( div1O && div2O && div3O ) {
+    alert("O`s win");
+  }else if( div4O && div5O && div6O ) {
+    alert("O`s win");
+  }else if( div7O && div8O && div9O ) {
+    alert("O`s win");
+  }else if( div1O && div4O && div7O ) {
+    alert("O`s win");
+  }else if( div2O && div5O && div8O ) {
+    alert("O`s win");
+  }else if( div3O && div6O && div9O ) {
+    alert("O`s win");
+  }else if( div1O && div5O && div9O ) {
+    alert("O`s win");
+  }else if( div3O && div5O && div7O ) {
+    alert("O`s win");
+  }
 }
